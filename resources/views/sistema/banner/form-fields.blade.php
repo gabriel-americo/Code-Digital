77eb90dc06aee528eaab26729d24cec93548ea5d
Gabriel <gabrielamerico90@gmail.com>
2018-03-14 21:13:46 -0300

    Sistema2018

--- resources/views/sistema/banner/form-fields.blade.php
+++ resources/views/sistema/banner/form-fields.blade.php
@@ -2,8 +2,18 @@
     <div class="form-group">
         @include('sistema.templates.formulario.input', ['label' => 'Nome', 'input' => 'nome', 'attributes' => ['class' => 'form-control']])
     </div>
-    <div class="form-group">
-        @include('sistema.templates.formulario.file', ['note' => 'Coloque uma imagem JPG.', 'label' => 'Imagem', 'file' => 'imagem'])
+    <div class="form-group ">
+        <div class="fileinput fileinput-new" data-provides="fileinput">
+            <div class="fileinput-preview thumbnail" data-trigger="fileinput" style="width: 200px; height: 150px;"> </div>
+            <div>
+                <span class="btn red btn-outline btn-file">
+                    <span class="fileinput-new"> Selecione a imagem </span>
+                    <span class="fileinput-exists"> Mudar </span>
+                    <input type="file" name="imagem"> </span>
+                <a href="javascript:;" class="btn red fileinput-exists" data-dismiss="fileinput"> Remove </a>
+            </div>
+        </div>
+        <p class="help-block"> Coloque uma imagem JPG. </p>
     </div>
 
     <div class="form-group">
@@ -15,7 +25,7 @@
             <div class="form-group">
                 @include('sistema.templates.formulario.input', ['label' => '', 'input' => 'data_fim', 'attributes' => ['class' => 'form-control', 'placeholder' => 'Data fim', 'id' => 'mask_date3']])
             </div>
-
+            <p class="help-block"> Caso seu banner seja promocional coloque a data que ele ficará no ar. </p>
         </div>
     </div>
 
